99ab711991bdf3fa94def46163e0d9e0570844f5
Chris Kacerguis <chris@cowdog.io>
2017-10-19 15:03:23 -0500

    working!

--- worker.js
+++ worker.js
@@ -4,27 +4,36 @@ const rp = require('request-promise-native')
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 mongoose.connect(process.env.DB_URL, { useMongoClient: true })
-// mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise
 mongoose.set('debug', true)
 
 var Server = require('./models/server')
 
 const stream = Server.find().cursor()
 stream.eachAsync(doc => {
-  rp({uri: doc.url, simple: false, resolveWithFullResponse: true})
-  .then(function (res) {
-    doc.last_check.response_code = res && res.statusCode
-    doc.last_check.time = Date.now()
-    doc.save()
+  return new Promise((resolve, reject) => {
+    rp({uri: doc.url, simple: false, resolveWithFullResponse: true})
+    .then(function (res) {
+      doc.last_check.response_code = res && res.statusCode
+      doc.last_check.time = Date.now()
+      doc.save().then(() => {
+        resolve()
+      })
+    })
+    .catch(function (err) {
+      doc.last_check.message = err
+      // doc.last_check.response_code = res && res.statusCode
+      doc.last_check.time = Date.now()
+      doc.save().then(() => {
+        resolve()
+      })
+    })
   })
-  .catch(function (err) {
-    doc.last_check.message = err
-    // doc.last_check.response_code = res && res.statusCode
-    doc.last_check.time = Date.now()
-    doc.save()
-  })
+})
+.then(() => {
+  mongoose.disconnect()
+  return Promise.resolve()
 })
 .catch(function (err) {
   if (err) console.log(err)
 })
-.then(() => mongoose.disconnect())
